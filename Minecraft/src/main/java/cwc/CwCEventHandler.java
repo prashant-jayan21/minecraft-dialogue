d8b0171a6f55fca248636898995cebf73f23bb4b
Anjali Narayan-Chen <cheezifreako@gmail.com>
2017-10-10 04:06:57 -0500

    OMG can now pick up and put down blocks just like in BlocksWorldgit add Minecraft/src/main/java/cwc/CwCEventHandler.java git add Minecraft/src/main/java/cwc/CwCEventHandler.java  or closely enough

--- Minecraft/src/main/java/cwc/CwCEventHandler.java
+++ Minecraft/src/main/java/cwc/CwCEventHandler.java
@@ -5,17 +5,26 @@ import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.item.EntityFallingBlock;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SPacketHeldItemChange;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.*;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CwCEventHandler {
 
+    public static SimpleNetworkWrapper network = NetworkRegistry.INSTANCE.newSimpleChannel("cwc");
     private static int DEFAULT_STACK_SIZE = 10;
 
     /**
@@ -45,7 +54,16 @@ public class CwCEventHandler {
         if (!event.getEntity().getEntityWorld().isRemote && event.getEntity() instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) event.getEntity();
             System.out.println("onEntitySpawn: "+player.getName());
-            if (player.inventory.getStackInSlot(0).isEmpty()) {
+
+            boolean empty = true;
+            for (int i = 0; i < InventoryPlayer.getHotbarSize(); i++) {
+                if (!player.inventory.getStackInSlot(i).isEmpty()) {
+                    empty = false;
+                    break;
+                }
+            }
+
+            if (empty) {
                 player.inventory.addItemStackToInventory(new ItemStack(StartupCommon.red, DEFAULT_STACK_SIZE));
                 player.inventory.addItemStackToInventory(new ItemStack(StartupCommon.orange, DEFAULT_STACK_SIZE));
                 player.inventory.addItemStackToInventory(new ItemStack(StartupCommon.yellow, DEFAULT_STACK_SIZE));
@@ -69,12 +87,49 @@ public class CwCEventHandler {
     @SubscribeEvent
     public void onPlayerClone(PlayerEvent.Clone event) {
         if (!event.getEntity().getEntityWorld().isRemote && event.getEntity() instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) event.getEntity();
+            EntityPlayer player = event.getEntityPlayer();
             System.out.println("onPlayerClone: "+player.getName());
             player.capabilities.allowFlying = true;
             player.capabilities.disableDamage = true;
             player.sendPlayerAbilities();
             System.out.println("\t-- flying capabilities ON, damage OFF");
+        }
+    }
+
+    /**
+     * Switches active hotbar slot to the item just picked up, or first empty slot if the item doesn't exist in hotbar.
+     * If, for some reason, the hotbar is full and the item doesn't exist in hotbar already, the currently held item isn't changed.
+     * @param event
+     */
+    @SubscribeEvent
+    public void onItemPickup(EntityItemPickupEvent event) {
+        if (!event.getEntity().getEntityWorld().isRemote && event.getEntity() instanceof EntityPlayerMP) {
+            EntityPlayerMP player = (EntityPlayerMP) event.getEntityPlayer();
+            System.out.println("Item " + event.getItem().getName() + " picked up by " + player.getName());
+
+            if (event.getItem().getEntityItem().getItem() != Items.AIR) {
+                int slot = player.inventory.getSlotFor(event.getItem().getEntityItem());
+                int empty = player.inventory.getFirstEmptyStack();
+                player.inventory.currentItem = InventoryPlayer.isHotbar(slot) ? slot : empty < 0 ? player.inventory.currentItem : empty;
+                player.connection.sendPacket(new SPacketHeldItemChange(player.inventory.currentItem));
+            }
+        }
+    }
+
+    /**
+     * Sets held item to first empty slot upon placing a block. If, for some reason, no hotbar slots are empty, then
+     * this does nothing.
+     * @param event
+     */
+    @SubscribeEvent
+    public void onBlockPlace(BlockEvent.PlaceEvent event) {
+        if (!event.getPlayer().getEntityWorld().isRemote && event.getPlayer() instanceof EntityPlayerMP) {
+            EntityPlayerMP player = (EntityPlayerMP) event.getPlayer();
+            System.out.println("Block "+event.getPlacedBlock().getBlock().getUnlocalizedName()+" placed by "+player.getName());
+
+            int empty = player.inventory.getFirstEmptyStack();
+            player.inventory.currentItem = empty < 0 ? player.inventory.currentItem : empty;
+            player.connection.sendPacket(new SPacketHeldItemChange(player.inventory.currentItem));
         }
     }
 
