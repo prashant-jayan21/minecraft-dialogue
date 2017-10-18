cac3778e5080366771d130efb5c6cf3f89f171d1
Prashant Jayannavar <prashant@wirelessprv-10-192-95-32.near.illinois.edu>
2017-10-18 15:49:09 -0500

    feat: One log to rule them all! BWAHAHAHA!

---
+++ build/install/Python_Examples/cwc_all_observations.py
@@ -0,0 +1,264 @@
+# Prototype:
+# Two humans - w/ abilities to chat and build block structures
+# Record all observations
+
+import MalmoPython
+import os
+import sys
+import time
+import json
+
+def safeStartMission(agent_host, my_mission, my_client_pool, my_mission_record, role, expId):
+    used_attempts = 0
+    max_attempts = 5
+    print("Calling startMission for role", role)
+    while True:
+        try:
+            # Attempt start:
+            agent_host.startMission(my_mission, my_client_pool, my_mission_record, role, expId)
+            break
+        except MalmoPython.MissionException as e:
+            errorCode = e.details.errorCode
+            if errorCode == MalmoPython.MissionErrorCode.MISSION_SERVER_WARMING_UP:
+                print("Server not quite ready yet - waiting...")
+                time.sleep(2)
+            elif errorCode == MalmoPython.MissionErrorCode.MISSION_INSUFFICIENT_CLIENTS_AVAILABLE:
+                print("Not enough available Minecraft instances running.")
+                used_attempts += 1
+                if used_attempts < max_attempts:
+                    print("Will wait in case they are starting up.", max_attempts - used_attempts, "attempts left.")
+                    time.sleep(2)
+            elif errorCode == MalmoPython.MissionErrorCode.MISSION_SERVER_NOT_FOUND:
+                print("Server not found - has the mission with role 0 been started yet?")
+                used_attempts += 1
+                if used_attempts < max_attempts:
+                    print("Will wait and retry.", max_attempts - used_attempts, "attempts left.")
+                    time.sleep(2)
+            else:
+                print("Other error:", e.message)
+                print("Waiting will not help here - bailing immediately.")
+                exit(1)
+        if used_attempts == max_attempts:
+            print("All chances used up - bailing now.")
+            exit(1)
+    print("startMission called okay.")
+
+def safeWaitForStart(agent_hosts):
+    print "Waiting for the mission to start..."
+    start_flags = [False for a in agent_hosts]
+    start_time = time.time()
+    time_out = 120  # Allow a two minute timeout.
+    while not all(start_flags) and time.time() - start_time < time_out:
+        states = [a.peekWorldState() for a in agent_hosts]
+        start_flags = [w.has_mission_begun for w in states]
+        errors = [e for w in states for e in w.errors]
+        if len(errors) > 0:
+            print("Errors waiting for mission start:")
+            for e in errors:
+                print(e.text)
+            print("Bailing now.")
+            exit(1)
+        time.sleep(0.1)
+        sys.stdout.write('.')
+    if time.time() - start_time >= time_out:
+        print("Timed out while waiting for mission to start - bailing.")
+        exit(1)
+    print()
+    print("Mission has started.")
+
+# Create one agent host for parsing:
+agent_hosts = [MalmoPython.AgentHost()]
+
+try:
+    agent_hosts[0].parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_hosts[0].getUsage())
+    exit(1)
+if agent_hosts[0].receivedArgument("help"):
+    print(agent_hosts[0].getUsage())
+    exit(0)
+
+# Set observation policy
+agent_hosts[0].setObservationsPolicy(MalmoPython.ObservationsPolicy.KEEP_ALL_OBSERVATIONS)
+
+# Create the other agent host
+agent_hosts += [MalmoPython.AgentHost()]
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+
+# Set up a client pool
+client_pool = MalmoPython.ClientPool()
+client_pool.add( MalmoPython.ClientInfo('127.0.0.1', 10000) )
+client_pool.add( MalmoPython.ClientInfo('127.0.0.1', 10001) )
+
+# Create mission xml
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+              <About>
+                <Summary>CWC Prototype</Summary>
+              </About>
+
+              <ServerSection>
+                <ServerInitialConditions>
+                  <Time>
+                    <StartTime>1000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                  </Time>
+                  <Weather>clear</Weather>
+                </ServerInitialConditions>
+                <ServerHandlers>
+                  <FlatWorldGenerator generatorString="3;241;1;"/>
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+
+              <AgentSection mode="Survival">
+                <Name>Builder</Name>
+                <AgentStart>
+                  <Placement x = "0" y = "1" z = "0"/>
+                </AgentStart>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                   <ObservationFromGrid>
+                     <Grid name="builder_grid" absoluteCoords="false">
+                       <min x="-1" y="0" z="-1"/>
+                       <max x="1" y="2" z="1"/>
+                     </Grid>
+                   </ObservationFromGrid>
+                   <ObservationFromChat/>
+                </AgentHandlers>
+              </AgentSection>
+
+              <AgentSection mode="Spectator">
+                <Name>Architect</Name>
+                <AgentStart>
+                  <Placement x = "0" y = "1" z = "-5"/>
+                </AgentStart>
+                <AgentHandlers/>
+              </AgentSection>
+            </Mission>'''
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+
+safeStartMission(agent_hosts[0], my_mission, client_pool, MalmoPython.MissionRecordSpec(), 0, "cwc_dummy_mission")
+safeStartMission(agent_hosts[1], my_mission, client_pool, MalmoPython.MissionRecordSpec(), 1, "cwc_dummy_mission")
+
+safeWaitForStart(agent_hosts)
+
+time.sleep(1)
+
+timed_out = False
+
+import numpy as np
+
+def getPerspectiveCoordinates(x, y, z, yaw, pitch):
+    # construct vector
+    v = np.matrix('{}; {}; {}'.format(x, y, z))
+    # construct rotation matrix
+    theta = np.radians(-1 * yaw)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.matrix('{} {} {}; {} {} {}; {} {} {}'.format(c, 0, -s, 0, 1, 0, s, 0, c))
+    # multiply
+    v_new = R * v
+    x_new = v_new.item(0)
+    y_new = v_new.item(1)
+    z_new = v_new.item(2)
+    return (x_new, y_new, z_new)
+
+def processObservation(observation, prev_blocks_state_abs, prev_dialog_state):
+    msg_timestamp = observation.timestamp
+    msg = observation.text
+    json_obj = json.loads(msg)
+    # print "-"*20
+    # print json_obj
+    # print "-"*20
+    builder_grid_rel = json_obj.get(u'builder_grid_block_info_relative', 0)
+    current_blocks_state_rel = list(filter(lambda x: x["type"] != "air", builder_grid_rel))
+    builder_grid_abs = json_obj.get(u'builder_grid_block_info', 0)
+    current_blocks_state_abs = list(filter(lambda x: x["type"] != "air", builder_grid_abs))
+    # # print "-"*20
+    # # print msg_timestamp
+    # # print current_blocks_state
+    # # print "-"*20
+    builder_yaw = json_obj.get(u'Yaw', 0)
+    builder_pitch = json_obj.get(u'Pitch', 0)
+    builder_x_pos = json_obj.get(u'XPos', 0)
+    builder_y_pos = json_obj.get(u'YPos', 0)
+    builder_z_pos = json_obj.get(u'ZPos', 0)
+    # # print yaw
+    # # print pitch
+    chat_observation = json_obj.get(u'Chat', [])
+    if current_blocks_state_abs != prev_blocks_state_abs or chat_observation != []:
+        print
+        print "-"*20
+        print "STATE:"
+
+        print "timestamp"
+        print msg_timestamp
+        print
+
+        print "builder abs position"
+        print "(x, y, z): " + str((builder_x_pos, builder_y_pos, builder_z_pos))
+        print "(yaw, pitch): " + str((builder_yaw, builder_pitch))
+
+        for block in current_blocks_state_rel:
+            perspective_coords = getPerspectiveCoordinates(block["x"], block["y"], block["z"], builder_yaw, builder_pitch)
+            absolute_coords = (block["x"], block["y"], block["z"])
+            print
+            print block["type"]
+            print "absolute coordinates"
+            print absolute_coords
+            print "perspective coordinates"
+            print perspective_coords
+        prev_blocks_state_abs = current_blocks_state_abs
+
+        current_dialog_state = prev_dialog_state + chat_observation
+        print
+        print "chat"
+        print current_dialog_state
+        prev_dialog_state = current_dialog_state
+
+        print "-"*20
+
+    return (prev_blocks_state_abs, prev_dialog_state)
+
+prev_blocks_state_abs = []
+prev_dialog_state = []
+while not timed_out:
+
+    for i in range(2):
+
+        ah = agent_hosts[i]
+        world_state = ah.getWorldState()
+
+        if world_state.is_mission_running == False:
+            timed_out = True
+
+        if i == 0 and world_state.is_mission_running and world_state.number_of_observations_since_last_state > 0:
+            for observation in world_state.observations:
+                (prev_blocks_state_abs, prev_dialog_state) = processObservation(observation, prev_blocks_state_abs, prev_dialog_state)
+            # processObservation(world_state.observations[-1])
+
+    time.sleep(1)
+
+print()
+
+print "Waiting for mission to end..."
+# Mission should have ended already, but we want to wait until all the various agent hosts
+# have had a chance to respond to their mission ended message.
+hasEnded = False
+while not hasEnded:
+    hasEnded = True # assume all good
+    sys.stdout.write('.')
+    time.sleep(0.1)
+    for ah in agent_hosts:
+        world_state = ah.getWorldState()
+        if world_state.is_mission_running:
+            hasEnded = False # all not good
+
+print "Mission ended"
+# Mission has ended.
+
+time.sleep(2)
