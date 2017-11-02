05df54adfdf41f55e9c2b647cc229df2a05f2191
Anjali Narayan-Chen <cheezifreako@gmail.com>
2017-11-02 14:31:54 -0500

    Transparent block now returns translucent block layer

--- Minecraft/src/main/java/cwc/CwCTransparentBlock.java
+++ Minecraft/src/main/java/cwc/CwCTransparentBlock.java
@@ -4,10 +4,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CwCTransparentBlock extends Block {
     public CwCTransparentBlock() {
@@ -15,12 +18,9 @@ public class CwCTransparentBlock extends Block {
         this.setBlockUnbreakable();
         this.setResistance(6000000.0F);
     }
-    
-    @Override
-    public int getLightOpacity(IBlockState state, IBlockAccess world, BlockPos pos) { return 0; }
 
-    @Override
-    public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face) { return false; }
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() { return BlockRenderLayer.TRANSLUCENT; }
 
     @Override
     public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) { return false; }
