f3e91f2e6a47ad9e6e414c8405dac818c497673c
Prashant Jayannavar <prashant@Prashants-MacBook-Pro.local>
2017-11-12 17:36:19 -0600

    feat: Add support for rounds

--- build/install/Python_Examples/cwc_session_driver.py
+++ build/install/Python_Examples/cwc_session_driver.py
@@ -1,32 +1,52 @@
 from multiprocessing import Pool
+import time
 from cwc_all_functions import cwc_all_obs_and_save_data
 
-# Read info for all missions
+# Number of rounds in one session
+num_rounds = 3
+num_missions_per_round = 2
 
-args_mission_1 = {
-    "lan": False,
-    "builder_ip_addr": None,
-    "builder_id": "1",
-    "architect_ip_addr": None,
-    "architect_id": "2",
-    "gold_config": None,
-}
+# Spawn worker processes
+pool = Pool(processes=num_missions_per_round)
 
-args_mission_2 = {
-    "lan": False,
-    "builder_ip_addr": None,
-    "builder_id": "3",
-    "architect_ip_addr": None,
-    "architect_id": "4",
-    "gold_config": None,
-}
+# Execute rounds
+for i in range(num_rounds):
 
-mission_args = [args_mission_1, args_mission_2]
+    print "\nROUND " + str(i) + " STARTED..."
 
-num_missions = len(mission_args)
+    # Read info for all missions
+    args_mission_1 = {
+        "lan": False,
+        "builder_ip_addr": None,
+        "builder_id": "1",
+        "architect_ip_addr": None,
+        "architect_id": "2",
+        "gold_config": None,
+    }
 
-# Spawn processes
+    args_mission_2 = {
+        "lan": False,
+        "builder_ip_addr": None,
+        "builder_id": "3",
+        "architect_ip_addr": None,
+        "architect_id": "4",
+        "gold_config": None,
+    }
 
-pool = Pool(processes=num_missions)
+    mission_args = [args_mission_1, args_mission_2]
 
-pool.map(cwc_all_obs_and_save_data, mission_args)
+    # Submit missions jobs to process pool
+    print "\nMISSIONS RUNNING..."
+    pool.map(cwc_all_obs_and_save_data, mission_args)
+
+    print "\nROUND " + str(i) + " ENDED..."
+    print "\nWAITING FOR CLIENT RESETS..."
+
+    # Wait for client restarts
+    time.sleep(120)
+
+# Cleanup worker processes
+pool.close()
+pool.join()
+
+print "\nSESSION COMPLETE!"
