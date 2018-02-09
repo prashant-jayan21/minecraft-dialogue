c4700329bd4c805341a6bd7055fd9b00f5e384d8
Prashant Jayannavar <paj3@illinois.edu>
2018-02-09 00:13:45 -0600

    fix: Reimplement aligner to fix logical flaw 😑

--- build/install/Python_Examples/cwc_aligner_new.py
+++ build/install/Python_Examples/cwc_aligner_new.py
@@ -1,24 +1,50 @@
 import re
 
-def align(architect_filenames, builder_filenames):
-    aligned_pairs = []
+def align_fixed(architect_filenames, builder_filenames):
+    all_filenames = architect_filenames + builder_filenames
 
-    for architect_filename in architect_filenames:
-        architect_timestamp, architect_action = get_key(architect_filename)
-        # select all builder screenshots with same action
-        builder_filenames_same_action = [f for f in builder_filenames if architect_action in f]
-        if not builder_filenames_same_action:
-            continue
-        # select closest builder screenshot in time
-        def f(builder_filename):
-            builder_timestamp, builder_action = get_key(builder_filename)
-            return abs(float(builder_timestamp) - float(architect_timestamp))
-        builder_filename = min(builder_filenames_same_action, key = f)
-        # update data structures
-        builder_filenames.remove(builder_filename)
-        aligned_pairs.append((architect_filename, builder_filename))
+    grouping = {}
+    for filename in all_filenames:
+        key = get_key(filename)[1]
+        if key not in grouping:
+            grouping[key] = []
+        grouping[key].append(filename)
+
+    aligned_pairs = get_pairs(grouping)
+
+    all_aligned_filenames = [item for pair in aligned_pairs for item in pair]
+    all_unaligned_filenames = set(all_filenames) - set(all_aligned_filenames)
+    print "UNALIGNED SCREENSHOTS: " + str(all_unaligned_filenames)
 
     return aligned_pairs
+
+def get_pairs(grouping):
+    aligned_pairs = []
+
+    for action, all_filenames_for_action in grouping.iteritems():
+        # split into builder and architect screenshots
+        architect_filenames = filter(lambda x: "Architect" in x, all_filenames_for_action)
+        builder_filenames = filter(lambda x: "Builder" in x, all_filenames_for_action)
+        # create graph
+        graph = []
+        for a in architect_filenames:
+            for b in builder_filenames:
+                weight = get_weight(a, b)
+                graph.append((a, b, weight))
+        # process graph
+        while graph:
+            # select edge with min weight
+            a_min, b_min, w_min = min(graph, key = lambda x: x[2])
+            aligned_pairs.append((a_min, b_min))
+            # deletions
+            graph = filter(lambda x: x[0] != a_min and x[1] != b_min, graph)
+
+    return aligned_pairs
+
+def get_weight(architect_filename, builder_filename):
+    architect_timestamp, architect_action = get_key(architect_filename)
+    builder_timestamp, builder_action = get_key(builder_filename)
+    return abs(float(architect_timestamp) - float(builder_timestamp))
 
 def get_key(filename):
     return (re.split(r"[-.]",filename)[0], re.split(r"[-.]",filename)[2])
@@ -29,10 +55,10 @@ import pprint
 architect_dir = "/Users/prashant/Desktop/B1-A2-blue-original-L-1517603132657_a"
 builder_dir = "/Users/prashant/Desktop/B1-A2-blue-original-L-1517603132657_b"
 
-architect_filenames = os.listdir(architect_dir)
-builder_filenames = os.listdir(builder_dir)
+architect_filenames = filter(lambda x: x.endswith(".png"), os.listdir(architect_dir))
+builder_filenames = filter(lambda x: x.endswith(".png"), os.listdir(builder_dir))
 
-results = align(architect_filenames, builder_filenames)
+results = align_fixed(architect_filenames, builder_filenames)
 
 pp = pprint.PrettyPrinter()
 pp.pprint(results)
