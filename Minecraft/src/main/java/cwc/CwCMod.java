e0613f151589ac5bf45773b98d5e10cb39c41210
Anjali Narayan-Chen <cheezifreako@gmail.com>
2018-02-19 15:59:46 -0600

    Increased number of inventory blocks holdable by Builder to 10.

--- Minecraft/src/main/java/cwc/CwCMod.java
+++ Minecraft/src/main/java/cwc/CwCMod.java
@@ -36,7 +36,7 @@ public class CwCMod {
 
 	public static boolean enableAIToggle = false;		// whether or not the original Malmo AI/Human toggle option is enabled
 	public static boolean unlimitedInventory = false;	// whether or not Builder has unlimited inventory of blocks
-	public static final int MAX_INVENTORY_SIZE = 5;		// maximum number of blocks that can be held at a time by the Builder (if limited inventory)
+	public static final int MAX_INVENTORY_SIZE = 10;	// maximum number of blocks that can be held at a time by the Builder (if limited inventory)
 	protected static int DEFAULT_STACK_SIZE = 1;		// stack sizes of blocks in inventory upon initialization of Builder (if unlimited inventory)
 	protected static int NUM_FIXED_VIEWERS = 4;
 
