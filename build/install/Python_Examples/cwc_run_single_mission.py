1bded78e90f9249773f31dd5bf5c70fc52572223
Anjali Narayan-Chen <cheezifreako@gmail.com>
2018-02-26 13:40:21 -0600

    Major refactoring -- deleted old scripts and renamed files.

--- build/install/Python_Examples/cwc_run_single_mission.py
+++ build/install/Python_Examples/cwc_run_single_mission.py
@@ -2,9 +2,9 @@
 # Two humans - w/ abilities to chat and build block structures
 # Record all observations
 
-import MalmoPython
 import os, sys, time, json, datetime, copy
-import numpy as np
+import MalmoPython, numpy as np
+from cwc_mission_utils import safeStartMission, safeWaitForStart
 
 # observation grid parameters
 x_min_obs = -10
@@ -32,64 +32,7 @@ y_max_goal = y_max_build # NOTE: Do not change this relation without thought!
 z_min_goal = z_min_build + displacement
 z_max_goal = z_max_build + displacement
 
-placements = ['<Placement x = "0" y = "8" z = "-10" pitch="40"/>', '<Placement x = "0" y = "8" z = "10" pitch="40" yaw="180"/>', '<Placement x = "10" y = "8" z = "0" pitch="40" yaw="90"/>', '<Placement x = "-10" y = "8" z = "0" pitch="40" yaw="-90"/>']
-
-def safeStartMission(agent_host, my_mission, my_client_pool, my_mission_record, role, expId):
-    used_attempts = 0
-    max_attempts = 5
-    print("Calling startMission for role", role)
-    while True:
-        try:
-            # Attempt start:
-            agent_host.startMission(my_mission, my_client_pool, my_mission_record, role, expId)
-            break
-        except MalmoPython.MissionException as e:
-            errorCode = e.details.errorCode
-            if errorCode == MalmoPython.MissionErrorCode.MISSION_SERVER_WARMING_UP:
-                print("Server not quite ready yet - waiting...")
-                time.sleep(2)
-            elif errorCode == MalmoPython.MissionErrorCode.MISSION_INSUFFICIENT_CLIENTS_AVAILABLE:
-                print("Not enough available Minecraft instances running.")
-                used_attempts += 1
-                if used_attempts < max_attempts:
-                    print("Will wait in case they are starting up.", max_attempts - used_attempts, "attempts left.")
-                    time.sleep(2)
-            elif errorCode == MalmoPython.MissionErrorCode.MISSION_SERVER_NOT_FOUND:
-                print("Server not found - has the mission with role 0 been started yet?")
-                used_attempts += 1
-                if used_attempts < max_attempts:
-                    print("Will wait and retry.", max_attempts - used_attempts, "attempts left.")
-                    time.sleep(2)
-            else:
-                print("Other error:", e.message)
-                print("Waiting will not help here - bailing immediately.")
-                exit(1)
-        if used_attempts == max_attempts:
-            print("All chances used up - bailing now.")
-            exit(1)
-    print("startMission called okay.")
-
-def safeWaitForStart(agent_hosts):
-    print "Waiting for the mission to start..."
-    start_flags = [False for a in agent_hosts]
-    start_time = time.time()
-    time_out = 120  # Allow a two minute timeout.
-    while not all(start_flags) and time.time() - start_time < time_out:
-        states = [a.peekWorldState() for a in agent_hosts]
-        start_flags = [w.has_mission_begun for w in states]
-        errors = [e for w in states for e in w.errors]
-        if len(errors) > 0:
-            print("Errors waiting for mission start:")
-            for e in errors:
-                print(e.text)
-            print("Bailing now.")
-            exit(1)
-        time.sleep(0.1)
-        sys.stdout.write('.')
-    if time.time() - start_time >= time_out:
-        print("Timed out while waiting for mission to start - bailing.")
-        exit(1)
-    print("\nMission has started.")
+fv_placements = ['<Placement x = "0" y = "8" z = "-8" pitch="40"/>', '<Placement x = "0" y = "8" z = "10" pitch="40" yaw="180"/>', '<Placement x = "10" y = "8" z = "0" pitch="40" yaw="90"/>', '<Placement x = "-10" y = "8" z = "0" pitch="40" yaw="-90"/>']
 
 def getPerspectiveCoordinates(x, y, z, yaw, pitch):
     # construct vector
@@ -298,15 +241,15 @@ def addFixedViewers(n):
         fvs += '''<AgentSection mode="Spectator"> 
                     <Name>FixedViewer'''+str(i+1)+'''</Name> 
                     <AgentStart> 
-                      ''' + placements[i] + '''
+                      ''' + fv_placements[i] + '''
                       </AgentStart> 
                     <AgentHandlers/> 
                   </AgentSection>
                 '''
     return fvs
 
-def cwc_all_obs_and_save_data(args):
-    print "Calling cwc_all_obs_and_save_data with args:", args, "\n"
+def cwc_run_mission(args):
+    print "Calling cwc_run_mission with args:", args, "\n"
     start_time = time.time()
 
     builder_ip = args["builder_ip_addr"]
