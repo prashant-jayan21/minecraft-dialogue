29b4aa76f8c8545868c115a4f0eb6fc692834433
Prashant Jayannavar <paj3@illinois.edu>
2018-03-06 01:28:16 -0600

    feat + fix: Support legacy data + Handle division-by-zero error

--- build/install/Python_Examples/cwc_metrics.py
+++ build/install/Python_Examples/cwc_metrics.py
@@ -1,11 +1,24 @@
 import os, json, argparse
 from os.path import join, isdir
 
-def process_missions(logs_root_dir):
+def process_missions(logs_root_dir, legacy):
     all_log_dirs = filter(lambda x: isdir(join(logs_root_dir, x)), os.listdir(logs_root_dir))
 
     for log_dir in all_log_dirs:
-        process_observations(join(logs_root_dir, log_dir))
+        if legacy:
+            process_observations_legacy(join(logs_root_dir, log_dir))
+        else:
+            process_observations(join(logs_root_dir, log_dir))
+
+def process_observations_legacy(logs_dir): # TODO: Phase this out eventually
+    all_sub_dirs = filter(lambda x: isdir(join(logs_dir, x)), os.listdir(logs_dir))
+
+    for sub_dir in all_sub_dirs:
+        all_json_filenames = filter(lambda x: x.endswith(".json"), os.listdir(join(logs_dir, sub_dir, "json")))
+        for json_filename in all_json_filenames:
+            metrics_dict = get_all_metrics(join(logs_dir, sub_dir, "json", json_filename))
+            with open(join(logs_dir, sub_dir, "json", json_filename[:-5] + "_metrics.json"), "w") as metrics:
+                json.dump(metrics_dict, metrics)
 
 def process_observations(logs_dir):
     metrics_dict = get_all_metrics(join(logs_dir, "observations.json")) # FIXME: Change this later to final json name
@@ -44,7 +57,10 @@ def get_turn_metrics(chat_history):
     # compute metrics
     num_utterances = len(chat_history)
     num_turns = len(chat_history_collapsed)
-    utterances_per_turn = float(num_utterances) / float(num_turns)
+    if num_turns == 0:
+        utterances_per_turn = 0.0
+    else:
+        utterances_per_turn = float(num_utterances) / float(num_turns)
 
     return {
         "num_utterances": num_utterances,
@@ -60,6 +76,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Process observations to compute various dialog metrics")
     parser.add_argument("logs_root_dir", help="Root directory for all log data")
+    parser.add_argument("--legacy", default=False, action="store_true", help="Whether or not this is legacy data")
     args = parser.parse_args()
 
-    process_missions(args.logs_root_dir)
+    process_missions(args.logs_root_dir, args.legacy)
