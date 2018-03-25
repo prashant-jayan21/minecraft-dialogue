3a09092e6a860aa61feb923d2bb9dd8950047b06
Prashant Jayannavar <paj3@illinois.edu>
2018-03-25 00:29:43 -0500

    refactor: Refactor a bunch of stuff

--- build/install/Python_Examples/cwc_curriculum_generator.py
+++ build/install/Python_Examples/cwc_curriculum_generator.py
@@ -1,23 +1,23 @@
 import csv
-import pprint
 import argparse
+from cwc_io_utils import *
 
-def generate_curriculums(database, people_specs):
+def generate_curriculums(configs_db, people_specs):
     """
     Args:
-        database: Base data
+        configs_db: List of all config info
         people_specs: List of dicts containing people pairs and number of simple and complex configs for each people pair
 
     Returns:
-        A list of curriculums
+        A list of gold config curriculums -- one for each people pair
     """
     curriculums = []
 
     for people_pair_specs in people_specs:
         curriculum = generate_curriculum(
-            database = database,
-            num_simple = people_pair_specs["num_simple"],
-            num_complex = people_pair_specs["num_complex"],
+            configs_db = configs_db,
+            num_simple = people_pair_specs["num_simple_configs"],
+            num_complex = people_pair_specs["num_complex_configs"],
             person_1 = people_pair_specs["person_1"],
             person_2 = people_pair_specs["person_2"]
         )
@@ -25,93 +25,61 @@ def generate_curriculums(database, people_specs):
 
     return curriculums
 
-def generate_curriculum(database, num_simple, num_complex, person_1, person_2):
+def generate_curriculum(configs_db, num_simple, num_complex, person_1, person_2):
     """
     Args:
-        database: Base data
+        configs_db: List of all config info
         num_simple: Number of simple configurations required
         num_complex: Number of complex configurations required
         person_1: ID of first person
         person_2: ID of second person
 
     Returns:
-        A dict with a curriculum of configurations and the updated database
-        Also updates database
+        A curriculum of gold configs as a list
+        Also updates configs DB globally
     """
 
     # split simple and complex configs into two sets
-    simple_configs = [x for x in database if x["type"] == "simple"]
-    complex_configs = [x for x in database if x["type"] == "complex"]
+    simple_configs = [x for x in configs_db if x["type"] == "simple"]
+    complex_configs = [x for x in configs_db if x["type"] == "complex"]
 
-    # remove those already seen by people
+    # remove configs already seen by people
     simple_configs = [x for x in simple_configs if person_1 not in x["people"] and person_2 not in x["people"]]
     complex_configs = [x for x in complex_configs if person_1 not in x["people"] and person_2 not in x["people"]]
 
-    # round robin selection
-    # sort
+    # sort configs by number of runs
     simple_configs_sorted = sorted(simple_configs, key = lambda x: x["runs"])
     complex_configs_sorted = sorted(complex_configs, key = lambda x: x["runs"])
-    #select
+
+    # select top-k least run configs
     simple_configs_selected = simple_configs_sorted[:num_simple]
     complex_configs_selected = complex_configs_sorted[:num_complex]
-    #concatenate
+
+    # concatenate simple and complex configs
     curriculum = map(lambda x: x["configuration"], simple_configs_selected + complex_configs_selected)
 
-    # update DB
-    for config in database:
+    # update configs DB to reflect globally
+    for config in configs_db:
         if config["configuration"] in curriculum:
             config["runs"] += 1
             config["people"] = config["people"] + [person_1, person_2]
 
     return curriculum
 
-def read_database(database_file):
-    all_configs = []
-    with open(database_file, 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            row["people"] = map(int, row["people"].split())
-            row["runs"] = int(row["runs"])
-            all_configs.append(row)
-
-    return all_configs
-
-def read_people_specs(people_specs_file):
-    all_people_specs = []
-    with open(people_specs_file, 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            for key in row:
-                row[key] = int(row[key])
-            all_people_specs.append(row)
-
-    return all_people_specs
-
-def write_curriculums(curriculums, people_specs):
-    i = 0
-    for (curriculum, people_pair_specs) in zip(curriculums, people_specs):
-        i += 1
-        with open('curriculum_' + str(i) + '.csv', 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames = ['gold file path', 'existing file path'])
-            writer.writeheader()
-            for config in curriculum:
-                writer.writerow({'gold file path': 'gold-configurations/' + config, 'existing file path': ''})
-            print "DONE WRITING " + 'curriculum_' + str(i) + '.csv' + " FOR PEOPLE PAIR " + str(people_pair_specs["person_1"]) + ", " + str(people_pair_specs["person_2"])
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate gold config curriculums for builder/architect pairs")
-    parser.add_argument("database_csv", help="File path of the spreadsheet (.csv) containing gold config info")
+    parser.add_argument("configs_db_csv", help="File path of the spreadsheet (.csv) DB containing all gold config info")
     parser.add_argument("people_specs_csv", help="File path of the spreadsheet (.csv) containing all people specs for a session")
     args = parser.parse_args()
 
-    # read database file
-    database = read_database(args.database_csv)
+    # read configs DB file
+    configs_db = read_configs_db(args.configs_db_csv)
 
     # read people specs file
     people_specs = read_people_specs(args.people_specs_csv)
 
     # generate curriculums
-    curriculums = generate_curriculums(database, people_specs)
+    curriculums = generate_curriculums(configs_db, people_specs)
 
     # write curriculums to files
     write_curriculums(curriculums, people_specs)
---
+++ build/install/Python_Examples/cwc_update_configs_db.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+import csv
+from cwc_io_utils import *
+
+def update_configs_db(configs_db, curriculums, people_pairs):
+    """
+    Args:
+        configs_db: List of all config info
+        curriculums: A list of gold config curriculums -- one for each people pair
+        people_specs: List of people pairs
+
+    Returns:
+        Nothing
+        Updates configs DB globally
+    """
+    for (curriculum, people_pair) in zip(curriculums, people_pairs):
+        update_configs_db_single(configs_db, curriculum, people_pair[0], people_pair[1])
+
+def update_configs_db_single(configs_db, curriculum, person_1, person_2):
+    """
+    Args:
+        configs_db: List of all config info
+        curriculum: A gold config curriculum
+        person_1: ID of first person
+        person_2: ID of second person
+
+    Returns:
+        Nothing
+        Updates configs DB globally
+    """
+    for config in configs_db:
+        if config["configuration"] in curriculum:
+            config["runs"] += 1
+            config["people"] = config["people"] + [person_1, person_2]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update spreadsheet (.csv) DB containing all gold config info")
+    parser.add_argument("configs_db_csv", help="File path of the spreadsheet (.csv) DB containing all gold config info")
+    parser.add_argument("people_specs_csv", help="File path of the spreadsheet (.csv) containing all people specs for a session")
+    parser.add_argument("--curriculums_dir", default=".", help="Directory containing the curriculum files as curriculum_i.csv (i = 1, 2, ...)")
+    args = parser.parse_args()
+
+    # read configs DB file
+    configs_db = read_configs_db(args.configs_db_csv)
+
+    # read people specs file
+    people_specs = read_people_specs(args.people_specs_csv)
+    people_pairs = map(lambda x: (x["person_1"], x["person_2"]), people_specs)
+
+    # read curriculum files
+    curriculums = []
+    for i in range(len(people_pairs)):
+        curriculum_file = os.path.join(args.curriculums_dir, 'curriculum_' + str(i + 1) + '.csv')
+        curriculum = read_curriculum(curriculum_file)
+        curriculums.append(curriculum)
+
+    # update db
+    update_configs_db(configs_db, curriculums, people_pairs)
+
+    # write updated db to file
+    write_configs_db(configs_db, args.configs_db_csv)
