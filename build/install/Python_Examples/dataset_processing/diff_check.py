2513cc46f8d4191e556104537a238e77b133784c
Prashant Jayannavar <paj3@illinois.edu>
2018-05-02 19:45:26 -0500

    feat: Add more top level driver stuff

--- build/install/Python_Examples/dataset_processing/diff_check.py
+++ build/install/Python_Examples/dataset_processing/diff_check.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-import os, json, re, sys
+import os, json, re, sys, argparse, csv
 from os.path import join, isdir
 sys.path.insert(0, '../config_diff_tool')
 from diff import get_diff
@@ -14,7 +14,7 @@ def get_built_config(observations_dict):
         observations_dict: The json observations data for a mission
 
     Returns:
-        The built config at the end of the mission
+        The built config at the end of the mission as a list of dicts -- one dict per block
     """
 
     built_config_raw = observations_dict["WorldStates"][-1]["BlocksInGrid"]
@@ -32,6 +32,14 @@ def get_built_config(observations_dict):
     return built_config
 
 def get_gold_config(gold_config_xml_file):
+    """
+    Args:
+        gold_config_xml_file: The XML file for a gold configuration
+
+    Returns:
+        The gold config as a list of dicts -- one dict per block
+    """
+
     with open(gold_config_xml_file) as f:
         all_lines = map(lambda t: t.strip(), f.readlines())
 
@@ -49,14 +57,32 @@ def get_gold_config(gold_config_xml_file):
 
     return gold_config
 
+def process_logs_dataset(logs_dataset_dir, gold_configs_dir):
+    all_logs_root_dirs = filter(lambda x: isdir(join(logs_dataset_dir, x)), os.listdir(logs_dataset_dir))
+
+    results = []
+
+    for logs_root_dir in all_logs_root_dirs:
+        results_for_logs_root_dir = process_logs_root_dir(join(logs_dataset_dir, logs_root_dir), gold_configs_dir)
+        results = results + results_for_logs_root_dir
+
+    return results
+
 def process_logs_root_dir(logs_root_dir, gold_configs_dir):
     all_log_dirs = filter(lambda x: isdir(join(logs_root_dir, x)), os.listdir(logs_root_dir))
 
+    results = []
+
     for log_dir in all_log_dirs:
-        diff_size = g(logs_root_dir, log_dir, gold_configs_dir)
-        print(log_dir)
-        print(diff_size)
-        print("\n")
+        print "Computing diff for " + logs_root_dir + "/" + log_dir + " ..."
+        diff_size = process_log_dir(logs_root_dir, log_dir, gold_configs_dir)
+        result = {
+            "mission": log_dir,
+            "diff_size": diff_size
+        }
+        results.append(result)
+
+    return results
 
 def process_log_dir(logs_root_dir, log_dir, gold_configs_dir):
     # get gold config
@@ -77,3 +103,20 @@ def process_log_dir(logs_root_dir, log_dir, gold_configs_dir):
     diff_size = len(diff["gold_minus_built"]) + len(diff["built_minus_gold"])
 
     return diff_size
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process observations to check diff between gold config and built config")
+    parser.add_argument("logs_dataset_dir", help="Root directory for entire logs dataset")
+    parser.add_argument("gold_configs_dir", help="Root directory for all gold configs")
+    args = parser.parse_args()
+
+    # process logs dataset and collect results
+    results = process_logs_dataset(args.logs_dataset_dir, args.gold_configs_dir)
+
+    # write results to DB
+    with open("diffs_db.csv", 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = ['mission','diff_size'])
+        writer.writeheader()
+        for mission_dict in results:
+            writer.writerow(mission_dict)
+        print "DONE WRITING " + "diffs_db.csv"
