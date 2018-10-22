6a3ed5de05ecf2c74bd09597a919c1dc735c4606
Prashant Jayannavar <paj3@illinois.edu>
2018-10-21 20:56:27 -0500

    feat: Add more analyses

--- build/install/Python_Examples/dataset_processing/actions_distributions_analysis.py
+++ build/install/Python_Examples/dataset_processing/actions_distributions_analysis.py
@@ -21,40 +21,66 @@ def process_log_dir(logs_root_dir, log_dir, gold_configs_dir):
 
 def process_dialog_states(observations_dict, gold_config):
     for i, world_state in enumerate(observations_dict["WorldStates"]):
+        print("STATE NUMBER: " + str(i) + "\n")
         built_config_raw = world_state["BlocksInGrid"]
         built_config = list(map(reformat_built_config_block, built_config_raw))
 
         _, everything_min = get_diff(gold_config = gold_config, built_config = built_config)
 
         minimal_diffs_gold_config_space = list(map(lambda x: x[0][1].diff_gold_config_space, everything_min))
-        probs = get_gold_config_distribution(gold_config, minimal_diffs_gold_config_space)
+        probs_gold = get_gold_config_distribution(gold_config, minimal_diffs_gold_config_space)
         print("placement probability distribution over gold config blocks:")
-        print(probs)
+        print(probs_gold)
         print("\n")
+
+        print("-" * 75)
 
         minimal_diffs_built_config_space = list(map(lambda x: x[0][1].diff_built_config_space, everything_min))
-        probs = get_built_config_distribution(built_config, minimal_diffs_built_config_space)
+        probs_built = get_built_config_distribution(built_config, minimal_diffs_built_config_space)
         print("removal probability distribution over built config blocks:")
-        print(probs)
+        print(probs_built)
         print("\n")
-        print(minimal_diffs_gold_config_space)
-        print("\n")
-        print(minimal_diffs_built_config_space)
 
-        next_placements = list(map(lambda x: x["gold_minus_built"], minimal_diffs_built_config_space))
+        print("-" * 75)
+
         # union
-        print(next_placements)
+        next_placements = list(map(lambda x: x["gold_minus_built"], minimal_diffs_built_config_space))
+
         all_next_placement_blocks = [item for sublist in next_placements for item in sublist]
         all_next_placement_blocks_reformatted = list(map(dict_to_tuple, all_next_placement_blocks))
         all_next_placement_blocks_reformatted_unique = set(all_next_placement_blocks_reformatted)
         all_next_placement_blocks_unique = list(map(dict, all_next_placement_blocks_reformatted_unique))
-        print(all_next_placement_blocks_unique)
-        probs = get_gold_config_distribution(all_next_placement_blocks_unique, minimal_diffs_built_config_space)
-        print("idk:")
-        print(probs)
-        print("\n")
 
-        print("-" * 50)
+        probs = get_gold_config_distribution(all_next_placement_blocks_unique, minimal_diffs_built_config_space)
+        print("placement probability distribution over all possible blocks to place next in built config space:")
+        print(probs)
+        
+        print("\n")
+        print("DECOMPOSITION -- CORRECT BLOCKS, BLOCKS TO REMOVE AND BLOCKS TO PLACE:")
+
+        built_config_and_probs_built = list(zip(built_config, probs_built))
+        built_blocks_correct = list(map(
+            lambda x: x[0],
+            list(filter(lambda x: x[1] == 0, built_config_and_probs_built))
+        ))
+        built_blocks_to_remove = list(map(
+            lambda x: x[0],
+            list(filter(lambda x: x[1] > 0, built_config_and_probs_built))
+        ))
+        print("\n")
+        print("built config -- correct blocks:")
+        print(built_blocks_correct)
+        print("\n")
+        print("built config -- all possible blocks to remove next:")
+        print(built_blocks_to_remove)
+        print("\n")
+        print("all possible blocks to place next -- in built config space:")
+        print(all_next_placement_blocks_unique)
+
+        print("\n\n")
+        print("*" * 100)
+        print("*" * 100)
+        print("\n\n")
 
 if __name__ == "__main__":
     logs_root_dir = "/Users/prashant/Work/cwc-minecraft-models/data/logs/data-3-30/logs"
