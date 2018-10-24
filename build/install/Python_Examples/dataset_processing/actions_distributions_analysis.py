87ad5f5f93617f58cd164cb02569719f4713ee30
Prashant Jayannavar <paj3@illinois.edu>
2018-10-24 18:55:32 -0500

    refactor: Make code simpler

--- build/install/Python_Examples/dataset_processing/actions_distributions_analysis.py
+++ build/install/Python_Examples/dataset_processing/actions_distributions_analysis.py
@@ -1,8 +1,9 @@
 import os, json, re, sys, argparse, csv
+from collections import Counter
 from os.path import join, isdir
 from diff_check import get_gold_config, reformat_built_config_block
 sys.path.insert(0, '../config_diff_tool')
-from diff import get_diff, get_gold_config_distribution, get_built_config_distribution, dict_to_tuple
+from diff import get_diff, get_gold_config_distribution, get_built_config_distribution, dict_to_tuple, build_region_specs
 
 def process_log_dir(logs_root_dir, log_dir, gold_configs_dir):
     # get gold config
@@ -11,12 +12,13 @@ def process_log_dir(logs_root_dir, log_dir, gold_configs_dir):
 
     gold_config = get_gold_config(config_xml_file)
 
-    # process dialog state by state
+    # get observations
     postprocessed_observations_file = join(logs_root_dir, log_dir, "postprocessed-observations.json")
 
     with open(postprocessed_observations_file) as observations:
         observations_dict = json.load(observations)
 
+    # process dialog state by state
     process_dialog_states(observations_dict, gold_config)
 
 def process_dialog_states(observations_dict, gold_config):
@@ -27,64 +29,105 @@ def process_dialog_states(observations_dict, gold_config):
 
         _, everything_min = get_diff(gold_config = gold_config, built_config = built_config)
 
-        minimal_diffs_gold_config_space = list(map(lambda x: x[0][1].diff_gold_config_space, everything_min))
-        probs_gold = get_gold_config_distribution(gold_config, minimal_diffs_gold_config_space)
-        print("placement probability distribution over gold config blocks:")
-        print(probs_gold)
-        print("\n")
-
-        print("-" * 75)
-
         minimal_diffs_built_config_space = list(map(lambda x: x[0][1].diff_built_config_space, everything_min))
-        probs_built = get_built_config_distribution(built_config, minimal_diffs_built_config_space)
-        print("removal probability distribution over built config blocks:")
-        print(probs_built)
-        print("\n")
 
-        print("-" * 75)
+        print(minimal_diffs_built_config_space)
+        print("\n\n")
 
-        # union
-        next_placements = list(map(lambda x: x["gold_minus_built"], minimal_diffs_built_config_space))
+        all_grid_locations = get_type_distributions(minimal_diffs_built_config_space, built_config)
 
-        all_next_placement_blocks = [item for sublist in next_placements for item in sublist]
-        all_next_placement_blocks_reformatted = list(map(dict_to_tuple, all_next_placement_blocks))
-        all_next_placement_blocks_reformatted_unique = set(all_next_placement_blocks_reformatted)
-        all_next_placement_blocks_unique = list(map(dict, all_next_placement_blocks_reformatted_unique))
-
-        probs = get_gold_config_distribution(all_next_placement_blocks_unique, minimal_diffs_built_config_space)
-        print("placement probability distribution over all possible blocks to place next in built config space:")
-        print(probs)
-        
-        print("\n")
-        print("DECOMPOSITION -- CORRECT BLOCKS, BLOCKS TO REMOVE AND BLOCKS TO PLACE:")
-
-        built_config_and_probs_built = list(zip(built_config, probs_built))
-        built_blocks_correct = list(map(
-            lambda x: x[0],
-            list(filter(lambda x: x[1] == 0, built_config_and_probs_built))
+        grid_locations_with_blocks = list(filter(
+            lambda x: x["grid_location"]["type"] != "empty",
+            all_grid_locations
         ))
-        built_blocks_to_remove = list(map(
-            lambda x: x[0],
-            list(filter(lambda x: x[1] > 0, built_config_and_probs_built))
+
+        empty_grid_locations_with_next_placements = list(filter(
+            lambda x: x["grid_location"]["type"] == "empty" and x["type_distribution"]["empty"] < 1.0,
+            all_grid_locations
         ))
-        print("\n")
-        print("built config -- correct blocks:")
-        print(built_blocks_correct)
-        print("\n")
-        print("built config -- all possible blocks to remove next:")
-        print(built_blocks_to_remove)
-        print("\n")
-        print("all possible blocks to place next -- in built config space:")
-        print(all_next_placement_blocks_unique)
+
+        import pprint
+        pp = pprint.PrettyPrinter()
+        print("grid_locations_with_blocks\n")
+        pp.pprint(grid_locations_with_blocks)
+        print("\n\n")
+        print("empty_grid_locations_with_next_placements\n")
+        pp.pprint(empty_grid_locations_with_next_placements)
 
         print("\n\n")
         print("*" * 100)
         print("*" * 100)
         print("\n\n")
+
+all_types = ["empty", "red", "orange", "green", "blue", "yellow", "purple"]
+
+def get_type_distributions(minimal_diffs_built_config_space, built_config):
+    results = []
+
+    all_x_values = [i for i in range(build_region_specs["x_min_build"], build_region_specs["x_max_build"] + 1)]
+    all_y_values = [i for i in range(build_region_specs["y_min_build"], build_region_specs["y_max_build"] + 1)]
+    all_z_values = [i for i in range(build_region_specs["z_min_build"], build_region_specs["z_max_build"] + 1)]
+
+    for x in all_x_values:
+        for y in all_y_values:
+            for z in all_z_values:
+                grid_location = {
+                    "x": x,
+                    "y": y,
+                    "z": z
+                }
+
+                occurence_in_built_config = next(
+                    (t for t in built_config if t["x"] == grid_location["x"] and t["y"] == grid_location["y"] and t["z"] == grid_location["z"]),
+                    None
+                )
+
+                if occurence_in_built_config:
+                    grid_location["type"] = occurence_in_built_config["type"]
+                else:
+                    grid_location["type"] = "empty"
+
+                types_acc_to_diffs = []
+
+                for diff in minimal_diffs_built_config_space:
+                    # what does this diff say about grid_location
+                    types_acc_to_diffs.append(get_type_acc_to_diff(grid_location, diff))
+
+                counts = Counter(types_acc_to_diffs)
+                type_distribution = {}
+                for type in all_types:
+                    type_distribution[type] = float(counts[type]) / float(len(minimal_diffs_built_config_space))
+
+                results.append(
+                    {
+                        "grid_location": grid_location,
+                        "type_distribution": type_distribution
+                    }
+                )
+
+    return results
+
+def get_type_acc_to_diff(grid_location, diff):
+    occurence_in_removals = next(
+        (t for t in diff["built_minus_gold"] if t["x"] == grid_location["x"] and t["y"] == grid_location["y"] and t["z"] == grid_location["z"]),
+        None
+    )
+
+    occurence_in_placements = next(
+        (t for t in diff["gold_minus_built"] if t["x"] == grid_location["x"] and t["y"] == grid_location["y"] and t["z"] == grid_location["z"]),
+        None
+    )
+
+    if occurence_in_removals is None and occurence_in_placements is None:
+        return grid_location["type"]
+    elif occurence_in_placements:
+        return occurence_in_placements["type"]
+    elif occurence_in_removals:
+        return "empty"
 
 if __name__ == "__main__":
-    logs_root_dir = "/Users/prashant/Work/cwc-minecraft-models/data/logs/data-3-30/logs"
-    log_dir = "B1-A3-C8-1522432497234"
+    logs_root_dir = "/Users/prashant/Work/cwc-minecraft-models/data/logs/data-4-5/logs"
+    log_dir = "B13-A30-C3-1522967809907"
     gold_configs_dir = "/Users/prashant/Work/cwc-minecraft-models/data/gold-configurations"
 
     process_log_dir(logs_root_dir, log_dir, gold_configs_dir)
