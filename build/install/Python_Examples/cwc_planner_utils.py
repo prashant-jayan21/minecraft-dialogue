a1cae97b7778c4fbbfde4770b9d3ed60b010870b
Rakib Islam <jahin114@gmail.com>
2019-03-25 14:29:22 -0700

    Planner jar reponse convert to python Response class
    Added a Response class with the same functions as before

--- build/install/Python_Examples/cwc_planner_utils.py
+++ build/install/Python_Examples/cwc_planner_utils.py
@@ -1,7 +1,127 @@
 from subprocess import *
+from enum import Enum
+
+
+class Response(object):
+    """Response classs.
+
+    Args:
+        responseFlag: the Flag names. Currently we are
+        using following flags
+        COMPLETED,
+        MISSING,
+        UNKNOWN,
+        SUGGESTION,
+        DUPLICATE_IDENTIFIER
+    Returns:
+        a response class
+    Raises:
+        till now nothing
+
+    """
+
+    def __init__(self, responseFlag, missing=None, other=None,
+                 plan=None, constraints=None):
+
+        self.responseFlag = responseFlag
+        self.missing = list()
+        self.other = list()
+        self.plan = list()
+        self.constraints = list()
+
+        if missing is not None:
+            self.missing = missing.split(",")
+
+        if other is not None:
+            self.other = other.split(",")
+
+        if plan is not None:
+            self.plan = plan.split(",")
+
+        if constraints is not None:
+            self.constraints = constraints.split(",")
+
+    def add_missing(self, missing):
+        """Add inforamtion about a missing logical form."""
+        self.missing.append(missing)
+
+    def add_missing_all(self, missing):
+        """Add inforamtion about all missing logical form."""
+        self.missing.extend(missing)
+
+    def add_other(self, other):
+        """Add inforamtion about others."""
+        self.other.append(other)
+
+    def add_constraints(self, constraints):
+        self.constraints.append(constraints)
+
+    def __str__(self):
+        content = {}
+        content['responseFlag'] = self.responseFlag
+        content['missing'] = self.missing
+        content['other'] = self.other
+        content['plan'] = self.plan
+        content['constraints'] = self.constraints
+        return str(content)
+
+
+def convert_Response(output):
+    """Convert the planner response to Response classs.
+
+    Args:
+        output: output from the planner. The general convention
+        reads the last 6 lines.
+    Returns:
+        the command list for the Malmo agent
+    Raises:
+        till now nothing
+
+    """
+    if len(output) < 5:
+        print("not enough information from the planner")
+    planner_output = output[-6:]
+    print(planner_output)
+    for line in planner_output:
+        if len(line) == 0:
+            continue
+        splitted_line = line.strip().split(": ")
+        if "[" in splitted_line[1]:
+            splitted_line[1] = splitted_line[
+                1].replace("[", "").replace("]", "")
+        print("splitted lines ", splitted_line)
+        if "Flag" in splitted_line[0]:
+            flag = splitted_line[1]
+        elif "Missing" in splitted_line[0]:
+            missing = splitted_line[1]
+        elif "Other" in splitted_line[0]:
+            other = splitted_line[1]
+        elif "Plan" in splitted_line[0]:
+            plan = splitted_line[1]
+        elif "Constraints" in splitted_line[0]:
+            constraints = splitted_line[1]
+
+    print("##parsed outputs##")
+    print(flag, missing, other, plan, constraints)
+    response = Response(flag, missing, other, plan, constraints)
+    print(response)
+    print(response.responseFlag)
+    response.add_constraints('hellow')
+    print(response)
+    print("##end of parsed outputs##")
 
 
 def jarWrapper(*args):
+    """A wrapper fuction that bridges python with Java based planner.
+
+    Args:
+        args: the arguments to pass to java program
+    Returns:
+        return the planner output.
+    Raises:
+
+
+    """
     process = Popen(['java', '-jar'] + list(args), stdout=PIPE, stderr=PIPE)
     ret = []
     while process.poll() is None:
@@ -18,12 +138,26 @@ def jarWrapper(*args):
 
 
 def getPlans(human_input="row(a) ^ width(a,5)"):
+    """Fetch the plans for a given semantic representation.
+
+    Args:
+        human_input: semantic representation format
+    Returns:
+        the command list for the Malmo agent
+    Raises:
+        till now nothing
+
+    """
     print("get plans")
     # args = ["planner/uct_44.jar"]
     args = ["planner/jshop2-master.jar", human_input]
     result = jarWrapper(*args)
-    print(result[-2])
+    print("-----begin o/p from planner----")
+    print(result[-6:])
+    print("-----end o/p from planner------")
+    convert_Response(result)
     print("planner is providing this result")
+    return
     instruction_list = result[-2].replace("[", "").replace("]", "")
     instruction_list = instruction_list.replace(
         ",", "").replace("(", "").replace(")", "")
@@ -34,11 +168,14 @@ def getPlans(human_input="row(a) ^ width(a,5)"):
     command_list = list()
     x_l = list()
     y_l = list()
+    z_l = list()
     for i in range(0, n_instr, 4):
         x_l.append(int(float(instruction_list[i + 2])))
         y_l.append(int(float(instruction_list[i + 3])))
+        # todo: Rakib when Mayukh is done with the 3D planner.
+        z_l.append(int(0))
 
-    command_list = zip(x_l, y_l)
+    command_list = zip(x_l, y_l, z_l)
     command_list.sort(key=lambda t: t[1], reverse=True)
     print(command_list)
 
@@ -47,5 +184,10 @@ def getPlans(human_input="row(a) ^ width(a,5)"):
 
 if __name__ == '__main__':
     # getPlans(human_input="cube(a) ^ width(a,5)")
+    print("*****solving \"row(a) ^ width(a,5)\"*****")
     getPlans(human_input="row(a) ^ width(a,5)")
+    print("*****solving missing \"row(a) ^ width(a,5)\"*****")
+    getPlans(human_input="row(a)")
+
+    print("*****solving \"rectangle(a) ^ height(a, 2) ^ width(a,4)\"*****")
     getPlans(human_input="rectangle(a) ^ height(a, 2) ^ width(a,4)")
