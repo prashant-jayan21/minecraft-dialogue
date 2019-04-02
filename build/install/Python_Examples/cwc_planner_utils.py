2d870ada51973d0d735a37625d8bfd209032cedd
Anjali Narayan-Chen <cheezifreako@gmail.com>
2019-04-02 18:04:26 -0500

    refactor cwc_planner_utils back to previous format before merge, accounting for 3D additions to planner

--- build/install/Python_Examples/cwc_planner_utils.py
+++ build/install/Python_Examples/cwc_planner_utils.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from subprocess import *
 from enum import Enum
 
@@ -6,8 +7,7 @@ class Response(object):
     """Response classs.
 
     Args:
-        responseFlag: the Flag names. Currently we are
-        using following flags
+        responseFlag: the Flag names. Currently we are using following flags:
         COMPLETED,
         MISSING,
         UNKNOWN,
@@ -30,48 +30,67 @@ class Response(object):
         self.constraints = list()
 
         if missing is not None:
-            self.missing = missing.split(",")
+            missing_list = list()
+
+            for item in missing:
+                missing_item = item.replace("(","").replace(")","")
+                missing_list.append(missing_item.strip().split())
+
+            self.missing = missing_list
 
         if other is not None:
-            self.other = other.split(",")
+            self.other = other
 
         if plan is not None:
-            self.plan = plan.split(",")
+            instruction_list = list()
+
+            for item in plan:
+                instruction = item.replace("[", "").replace("]", "")
+                instruction = instruction.replace(",", "").replace("(", "").replace(")", "")
+
+                if "stack" in instruction.strip():
+                    action, block_id, reference_block_id, x, y, z, color = instruction.strip().split()
+                else:
+                    action, block_id, x, y, z, color = instruction.strip().split()
+
+                instruction = [action, float(x), float(y), float(z), color]
+                instruction_list.append(instruction)
+
+            self.plan = instruction_list
 
         if constraints is not None:
-            self.constraints = constraints.split(",")
+            self.constraints = constraints
 
     def add_missing(self, missing):
-        """Add inforamtion about a missing logical form."""
+        """Add information about a missing logical form."""
         self.missing.append(missing)
 
     def add_missing_all(self, missing):
-        """Add inforamtion about all missing logical form."""
+        """Add information about all missing logical forms."""
         self.missing.extend(missing)
 
     def add_other(self, other):
-        """Add inforamtion about others."""
+        """Add information about others."""
         self.other.append(other)
 
     def add_constraints(self, constraints):
         self.constraints.append(constraints)
 
     def __str__(self):
-        content = {}
-        content['responseFlag'] = self.responseFlag
-        content['missing'] = self.missing
-        content['other'] = self.other
-        content['plan'] = self.plan
-        content['constraints'] = self.constraints
-        return str(content)
+        return_str = ""
+        return_str += "Response flag: "+self.responseFlag+"\n"
+        return_str += "Plan: "+str(self.plan)+"\n"
+        return_str += "Constraints: "+str(self.constraints)+"\n"
+        return_str += "Missing: "+str(self.missing)+"\n"
+        return_str += "Other: "+str(self.other)+"\n"
+        return return_str
 
-
-def convert_Response(output):
-    """Convert the planner response to Response classs.
+def convert_response(output):
+    """ 
+    Convert the planner response to Response class.
 
     Args:
-        output: output from the planner. The general convention
-        reads the last 6 lines.
+        output: output from the planner. The general convention reads the last 6 lines.
     Returns:
         the command list for the Malmo agent
     Raises:
@@ -79,38 +98,40 @@ def convert_Response(output):
 
     """
     if len(output) < 5:
-        print("not enough information from the planner")
+        print("convert_response::Error: not enough information from the planner")
+        return None
+
     planner_output = output[-6:]
-    print(planner_output)
+    contents = {"Missing": [], "Other": [], "Plan": [], "Constraints": []}
+    print("convert_response::", planner_output)
+
     for line in planner_output:
         if len(line) == 0:
             continue
+        
         splitted_line = line.strip().split(": ")
         if "[" in splitted_line[1]:
-            splitted_line[1] = splitted_line[
-                1].replace("[", "").replace("]", "")
-        # print("splitted lines ", splitted_line)
-        if "Flag" in splitted_line[0]:
-            flag = splitted_line[1]
-        elif "Missing" in splitted_line[0]:
-            missing = splitted_line[1]
-        elif "Other" in splitted_line[0]:
-            other = splitted_line[1]
-        elif "Plan" in splitted_line[0]:
-            plan = splitted_line[1]
-        elif "Constraints" in splitted_line[0]:
-            constraints = splitted_line[1]
+            splitted_line[1] = splitted_line[1].replace("[", "").replace("]", "")
+
+        res = splitted_line[1].split(",") if len(splitted_line[1]) > 0 else []
+        key = splitted_line[0].strip()
+
+        if "Flag" in key:
+            flag = res[0]
+        elif key in contents:
+            contents[key] = res
+        else:
+            print("convert_response::Error: unexpected planner output", line)
 
     # print("##parsed outputs##")
     # print(flag, missing, other, plan, constraints)
-    response = Response(flag, missing, other, plan, constraints)
+    response = Response(flag, contents["Missing"], contents["Other"], contents["Plan"], contents["Constraints"])
     # print(response)
     # print(response.responseFlag)
     # response.add_constraints('hellow')
     # print(response)
     # print("##end of parsed outputs##")
     return response
-
 
 def jarWrapper(*args):
     """A wrapper fuction that bridges python with Java based planner.
@@ -124,22 +145,27 @@ def jarWrapper(*args):
 
     """
     process = Popen(['java', '-jar'] + list(args), stdout=PIPE, stderr=PIPE)
+    
     ret = []
     while process.poll() is None:
         line = str(process.stdout.readline())
         if line != '' and line.endswith('\n'):
             ret.append(line[:-1])
+    
     stdout, stderr = process.communicate()
     ret += str(stdout).split('\n')
+    
     if stderr != '':
         ret += (str(stderr)).split('\n')
+    
     if '' in ret:
         ret.remove('')
+    
     return ret
 
-
 def getPlans(human_input="row(a) ^ width(a,5)"):
-    """Fetch the plans for a given semantic representation.
+    """
+    Fetch the plans for a given semantic representation.
 
     Args:
         human_input: semantic representation format
@@ -149,47 +175,12 @@ def getPlans(human_input="row(a) ^ width(a,5)"):
         till now nothing
 
     """
-    print("get plans")
-    # args = ["planner/uct_44.jar"]
     args = ["jshop2-master.jar", human_input]
     result = jarWrapper(*args)
-    # print(result)
-    response = convert_Response(result)
-    print(response.responseFlag)
-    # TODO:
-    if "COMPLETED" not in response.responseFlag:
-        return
-    instruction_list = list()
-    # print(response.plan)
-    for item in response.plan:
-        instruction = item.replace("[", "").replace("]", "")
-        instruction = instruction.replace(
-            ",", "").replace("(", "").replace(")", "")
-        instruction_list.append(instruction)
 
-    # print(instruction_list)
-    n_instr = len(instruction_list)
-
-    command_list = list()
-    x_l = list()
-    y_l = list()
-    z_l = list()
-    for item in instruction_list:
-        print("item ", item)
-        if "stack" in item.strip():
-            action, block_id, reference_block_id, x, y, z, color = item.strip().split(" ")
-        else:
-            action, block_id, x, y, z, color = item.strip().split(" ")
-        x_l.append(int(float(x)))
-        y_l.append(int(float(y)))
-        # todo: Rakib when Mayukh is done with the 3D planner.
-        z_l.append(int(float(z)))
-
-    command_list = zip(x_l, y_l, z_l)
-    command_list.sort(key=lambda t: t[1], reverse=True)
-    # print(command_list)
-
-    return (command_list)
+    response = convert_response(result)
+    print("getPlans::\n"+str(response))
+    return response
 
 
 if __name__ == '__main__':
