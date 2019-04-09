6fa595dd1a0e27de829f64bba2b3d3d76c7ca8ca
Prashant Jayannavar <paj3@illinois.edu>
2019-04-08 23:50:51 -0700

    feat: Integrate model fully

--- build/install/Python_Examples/cwc_run_single_mission.py
+++ build/install/Python_Examples/cwc_run_single_mission.py
@@ -3,13 +3,15 @@
 # Record all observations
 
 import os, sys, time, json, datetime, copy, pprint
+from os.path import join
 from argparse import Namespace
 import MalmoPython, numpy as np
 import cwc_mission_utils as mission_utils, cwc_debug_utils as debug_utils, cwc_io_utils as io_utils
-from cwc_postprocess_observations import reformatObservations, mergeObservations
+from cwc_postprocess_observations import reformatObservations, mergeObservations, postprocess
 
 sys.path.append('../../../../cwc-minecraft-models/python')
 import generate_seq2seq_online
+from utils import get_gold_config
 
 def addFixedViewers(n):
     fvs = ''
@@ -195,6 +197,33 @@ def cwc_run_mission(args):
 
     mission_utils.safeWaitForStart(agent_hosts)
 
+    # model and gold config xml loading
+    args2 = Namespace(
+        model_dir = '/Users/prashant/Work/cwc-minecraft-models/models_remote/utterances_and_block_region_counters/20190227/utterances_and_block_region_counters_trainer-1551313181240/1551313181248/',
+        data_dir = '/Users/prashant/Work/cwc-minecraft-models/data/logs/',
+        gold_configs_dir = '/Users/prashant/Work/cwc-minecraft-models/data/gold-configurations/',
+        saved_dataset_dir = None,
+        vocab_dir = "/Users/prashant/Work/cwc-minecraft-models/vocabulary/",
+        output_path = None,
+        num_workers = 0,
+        seed = 1234,
+        beam_size = 10,
+        max_decoding_length = 50,
+        development_mode = False,
+        decoding_strategy = 'beam',
+        gamma = 0.8,
+        regenerate_sentences = True,
+        model_iteration = 'best',
+        split = 'val',
+        disable_shuffle = False
+    )
+
+    config_name = args["gold_config"].split("/")[-1].split(".")[0]
+    config_xml_file = join(args2.gold_configs_dir, config_name + ".xml")
+    config_structure = get_gold_config(config_xml_file)
+
+    config_params, models, encoder_vocab, decoder_vocab = generate_seq2seq_online.setup(args2)
+
     # poll for observations
     timed_out = False
     all_observations = []
@@ -211,17 +240,16 @@ def cwc_run_mission(args):
                 for observation in world_state.observations:
                     total_elements += len(json.loads(observation.text))
 
-                print(("Received", len(world_state.observations), "observations. Total number of elements:", total_elements))
+                # print(("Received", len(world_state.observations), "observations. Total number of elements:", total_elements))
                 for observation in world_state.observations:
-                    print("Processing observation:", end=' ')
-                    debug_utils.printObservationElements(json.loads(observation.text))
+                    # print("Processing observation:", end=' ')
+                    # debug_utils.printObservationElements(json.loads(observation.text))
                     # pprint.PrettyPrinter(indent=4).pprint(json.loads(observation.text))
                     all_observations.append(observation)
 
                 for observation in world_state.observations:
-                    if json.loads(observation.text).get('Chat') == ['<Builder> XXX']:
+                    if json.loads(observation.text).get('Chat') == ['<Builder> xxx']:
                         print("Speak Architect")
-                        # agent_hosts[2].sendCommand("chat hahaha")
 
                         def f(all_observations):
                             all_world_states = []
@@ -238,34 +266,14 @@ def cwc_run_mission(args):
 
                         reformatted = reformatObservations(all_world_states)
                         all_world_states_merged = mergeObservations(reformatted)
+                        string_to_write = postprocess(all_world_states_merged, False)
 
                         log = {}
                         log["WorldStates"] = all_world_states_merged
 
-                        config_name = args["gold_config"].split("/")[-1].split(".")[0]
-
-                    	# parser.add_argument('--data_dir', type=str, default='../data/logs/', help='path for data jsons')
-                    	# parser.add_argument('--gold_configs_dir', type=str, default='../data/gold-configurations/', help='path for gold config xmls')
-                    	# parser.add_argument('--saved_dataset_dir', type=str, default=None, help='path for saved dataset to use')
-                    	# parser.add_argument('--vocab_dir', type=str, default="../vocabulary/", help='path for vocabulary files')
-                    	# parser.add_argument('--output_path', type=str, default=None, help='path for output file of generated sentences')
-                    	# parser.add_argument('--num_workers', type=int, default=0, help='number of workers')
-                    	# parser.add_argument('--seed', type=int, default=1234, help='random seed')
-                    	# parser.add_argument('--beam_size', type=int, default=10, help='beam size for beam search decoding')
-                    	# parser.add_argument('--max_decoding_length', type=int, default=50, help='max iterations for decoder when decoding w/o ground truth inputs') # FIXME: Do not use hard coded string
-                    	# parser.add_argument("--development_mode", default=False, action="store_true", help="Whether or not to run in development mode, i.e., with less data")
-                    	# parser.add_argument('--decoding_strategy', type=str, default='beam', help='multinomial/beam')
-                    	# parser.add_argument('--gamma', type=float, default=0.8, help='gamma penalty')
-                    	# parser.add_argument('--regenerate_sentences', default=False, action='store_true', help='generate sentences for a model even if a generated sentences file already exists in its directory')
-                    	# parser.add_argument('--model_iteration', default='best', help='iteration of model to be evaluated: "best" or "final"')
-                    	# parser.add_argument('--split', default='val', help='data split from which sentences should be generated')
-                    	# parser.add_argument('--disable_shuffle', default=False, action='store_true', help='disable shuffling of the data to be generated from')
-
-                        args2 = Namespace(
-                            model_dir = '../models/utterances_and_block_counters/20190223/utterances_and_block_counters_trainer-1550960969400/1550960969401/'
-                        )
-
-                        generate_seq2seq_online.main(args2, config_name, log)
+                        # pprint.PrettyPrinter(indent=4).pprint(log)
+                        gen_architect_utterance = generate_seq2seq_online.predict(args2, config_name, config_structure, log, config_params, models, encoder_vocab, decoder_vocab)
+                        agent_hosts[2].sendCommand("chat " + gen_architect_utterance)
 
                         all_observations = all_observations[:-1 * len(world_state.observations)]
 
